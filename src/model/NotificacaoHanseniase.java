c932dd75263e8c3ba8ebfab128ef087224bf4315
Ray <rayscampos00@gmail.com>
2025-10-31 14:27:49 -0300

    Medium changes

--- src/model/NotificacaoHanseniase.java
+++ src/model/NotificacaoHanseniase.java
@@ -1,19 +1,42 @@
 package model;
 
-import enums.agravos.hanseniase.FormaClinica;
+import java.time.LocalDate;
+import enums.agravos.hanseniase.*;
 
 public class NotificacaoHanseniase extends Notificacao {
     private String numeroProntuario;
     private String ocupacao;
     private Integer numeroLesoes;
     private FormaClinica formaClinica;
+    private ClassificacaoOperacional classificacaoOperacional;
+    private Integer numeroNervos;
+    private GrauIncapacidade grauIncapacidade;
+    private ModoEntrada modoEntrada;
+    private ModoDeteccaoCaso modoDeteccaoCaso;
+    private Baciloscopia baciloscopia;
+    private LocalDate dataInicioTratamento;
+    private EsquemaTerapeutico esquemaTerapeutico;
+    private Integer numeroContatosRegistrados;
 
     public NotificacaoHanseniase(
         DadosGerais dadosGerais,
-        DadosInvestigador dadosInvestigador, 
+        DadosInvestigador dadosInvestigador,
         DadosPaciente dadosPaciente, 
         DadosResidenciais dadosResidenciais,
-        Conclusao conclusao){
+        Conclusao conclusao,
+        String numeroProntuario,
+        String ocupacao,
+        Integer numeroLesoes,
+        FormaClinica formaClinica,
+        ClassificacaoOperacional classificacaoOperacional,
+        Integer numeroNervos,
+        GrauIncapacidade grauIncapacidade,
+        ModoEntrada modoEntrada,
+        ModoDeteccaoCaso modoDeteccaoCaso,
+        Baciloscopia baciloscopia,
+        LocalDate dataInicioTratamento,
+        EsquemaTerapeutico esquemaTerapeutico,
+        Integer numeroContatosRegistrados){
 
         super(dadosGerais, dadosInvestigador, dadosPaciente, dadosResidenciais, conclusao);
     }
