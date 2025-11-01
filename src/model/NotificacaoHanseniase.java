60b034e3613e469793a2d249b580b04bf8c9eea3
Ray <rayscampos00@gmail.com>
2025-11-01 14:00:18 -0300

    Full class- getters/setters

--- src/model/NotificacaoHanseniase.java
+++ src/model/NotificacaoHanseniase.java
@@ -39,5 +39,121 @@ public class NotificacaoHanseniase extends Notificacao {
         Integer numeroContatosRegistrados){
 
         super(dadosGerais, dadosInvestigador, dadosPaciente, dadosResidenciais, conclusao);
+        this.numeroProntuario = numeroProntuario;
+        this.ocupacao = ocupacao;
+        this.numeroLesoes = numeroLesoes;
+        this.formaClinica = formaClinica;
+        this.classificacaoOperacional = classificacaoOperacional;
+        this.numeroNervos = numeroNervos;
+        this.grauIncapacidade = grauIncapacidade;
+        this.modoEntrada = modoEntrada;
+        this.modoDeteccaoCaso = modoDeteccaoCaso;
+        this.baciloscopia = baciloscopia;
+        this.dataInicioTratamento = dataInicioTratamento;
+        this.esquemaTerapeutico = esquemaTerapeutico;
+        this.numeroContatosRegistrados = numeroContatosRegistrados;
+    }
+
+    public String getNumeroProntuario() {
+        return numeroProntuario;
+    } 
+    
+    public void setNumeroProntuario(String numeroProntuario) {
+        this.numeroProntuario = numeroProntuario;
+    }
+    
+    public String getOcupacao() {
+        return ocupacao;
+    } 
+    
+    public void setOcupacao(String ocupacao) {
+        this.ocupacao = ocupacao;
+    }
+
+    public Integer getNumeroLesoes() {
+        return numeroLesoes;
+    } 
+    
+    public void setNumeroLesoes(Integer numeroLesoes) {
+        this.numeroLesoes = numeroLesoes;
+    }
+
+    public FormaClinica getFormaClinica() {
+        return formaClinica;
+    } 
+    
+    public void setFormaClinica(FormaClinica formaClinica) {
+        this.formaClinica = formaClinica;
+    }
+    
+    public ClassificacaoOperacional getClassificacaoOperacional() {
+        return classificacaoOperacional;
+    } 
+    
+    public void setClassificacaoOperacional(ClassificacaoOperacional classificacaoOperacional) {
+        this.classificacaoOperacional = classificacaoOperacional;
+    }
+    
+    public Integer getNumeroNervos() { 
+        return numeroNervos; 
+    } 
+    
+    public void setNumeroNervos(Integer numeroNervos) { 
+        this.numeroNervos = numeroNervos; 
+    }
+    
+    public GrauIncapacidade getGrauIncapacidade() { 
+        return grauIncapacidade; 
+    } 
+    public void setGrauIncapacidade(GrauIncapacidade grauIncapacidade) { 
+        this.grauIncapacidade = grauIncapacidade; 
+    }
+   
+    public ModoEntrada getModoEntrada() { 
+        return modoEntrada; 
+    } 
+    
+    public void setModoEntrada(ModoEntrada modoEntrada) { 
+        this.modoEntrada = modoEntrada; 
+    }
+    
+    public ModoDeteccaoCaso getModoDeteccaoCaso() { 
+        return modoDeteccaoCaso; 
+    } 
+    
+    public void setModoDeteccaoCaso(ModoDeteccaoCaso modoDeteccaoCaso) { 
+        this.modoDeteccaoCaso = modoDeteccaoCaso; 
+    }
+    
+    public Baciloscopia getBaciloscopia() { 
+        return baciloscopia; 
+    } 
+    
+    public void setBaciloscopia(Baciloscopia baciloscopia) { 
+        this.baciloscopia = baciloscopia; 
+    }
+    
+    public LocalDate getDataInicioTratamento() { 
+        return dataInicioTratamento; 
+    } 
+    
+    public void setDataInicioTratamento(LocalDate dataInicioTratamento) { 
+        this.dataInicioTratamento = dataInicioTratamento; 
+    }
+    
+    public EsquemaTerapeutico getEsquemaTerapeutico() { 
+        return esquemaTerapeutico; 
+    } 
+    
+    public void setEsquemaTerapeutico(EsquemaTerapeutico esquemaTerapeutico) { 
+        this.esquemaTerapeutico = esquemaTerapeutico; 
+    }
+    
+    public Integer getNumeroContatosRegistrados() { 
+        return numeroContatosRegistrados; 
+    } 
+    
+    public void setNumeroContatosRegistrados(Integer numeroContatosRegistrados) { 
+        this.numeroContatosRegistrados = numeroContatosRegistrados; 
     }
 }
