5bc5ecb925add26eaec064c843fe232f21647f80
Ray <rayscampos00@gmail.com>
2025-11-06 16:53:07 -0300

    TipoPopulacao enum

---
+++ src/enums/agravos/tuberculose/TipoPopulacao.java
@@ -0,0 +1,11 @@
+package enums.agravos.tuberculose;
+
+import enums.EnumInterface;
+import util.EnumUtil;
+
+public enum TipoPopulacao implements EnumInterface{
+    PRIVADA_LIBERDADE(1, "População Privada de Liberdade"),
+    PROFISSIONAL_SAUDE(2, "Profissional de Saúde"),
+    SITUACAO_RUA(3, "População em Situação de Rua"),
+    IMIGRANTE(4, "Imigrante");
+}
