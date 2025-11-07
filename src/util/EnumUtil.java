9c1be64e2b6f9f5f61c967d2f887233da04212ca
Ray <rayscampos00@gmail.com>
2025-11-07 13:02:57 -0300

    New method added

--- src/util/EnumUtil.java
+++ src/util/EnumUtil.java
@@ -9,4 +9,19 @@ public class EnumUtil {
         for(E e : enumClass.getEnumConstants()) if (e.getCodigo() == codigo) return e;
         return valorPadrao;
     }
+
+    public static <E extends Enum<E> & EnumInterface> E escolherEnum(Class<E> enumClass) {
+        while(1) {
+            System.out.printf("Opções:\n");
+            for(E e : enumClass.getEnumConstants()) {
+                System.out.printf("%d - %s\n", e.getCodigo(), e.getDescricao());
+            }
+
+            int codigo = InputUtil.getInt("Escolha uma opção: ");
+            E en = EnumUtil.getFromCodigo(enumClass, codigo, null);
+
+            if(en != null) return en;
+            System.out.printf("Opçõa inválida!\n");
+        }
+    }
 }
