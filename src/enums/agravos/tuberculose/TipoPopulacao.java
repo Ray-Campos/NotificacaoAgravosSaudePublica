af44abf348bb78e6ccebe9714999c52cfc06ab05
Ray <rayscampos00@gmail.com>
2025-11-07 14:21:34 -0300

    Deprecated enum

--- src/enums/agravos/tuberculose/TipoPopulacao.java
+++ src/enums/agravos/tuberculose/TipoPopulacao.java
@@ -3,9 +3,9 @@ package enums.agravos.tuberculose;
 import enums.EnumInterface;
 import util.EnumUtil;
 
-public enum TipoPopulacao implements EnumInterface{
+/*public enum TipoPopulacao implements EnumInterface{
     PRIVADA_LIBERDADE(1, "População Privada de Liberdade"),
     PROFISSIONAL_SAUDE(2, "Profissional de Saúde"),
     SITUACAO_RUA(3, "População em Situação de Rua"),
     IMIGRANTE(4, "Imigrante");
-}
+}*/
