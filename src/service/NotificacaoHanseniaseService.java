fa1ba3c67d8cf72e15584290a24d9ed86ffbe176
Ray <rayscampos00@gmail.com>
2025-11-08 17:27:42 -0300

    feat: new blank NotificacaoHanseniaseService

---
+++ src/service/NotificacaoHanseniaseService.java
@@ -0,0 +1,12 @@
+package service;
+
+import service.NotificacaoService;
+import java.time.LocalDate;
+import enums.agravos.hanseniase.*;
+import model.NotificacaoHanseniase;
+
+public class NotificacaoHanseniaseService {
+    public static NotificacaoHanseniase cadastrar() {
+        
+    }
+}
